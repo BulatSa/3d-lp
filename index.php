37a53a27ee2f6c47971d2c3fd21767f1a76a8dd4
Bulat <sadikovba@gmail.com>
2018-07-26 17:02:36 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,250 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-sec">
+	<div class="header container">
+		<div class="header__logo">
+			<a href="#">
+				<img src="img/logo-top.svg" alt="">
+			</a>
+		</div>
+		<div class="header__about">
+			<div class="header__about-img">
+				<img src="img/header/logo-about.png" alt="">
+			</div>
+			<span>Официальный дистрибьютор</span>
+		</div>
+		<div class="header__garant">
+			<img src="img/header/icon-1.png" alt="">
+			<div class="header__garant-info">
+				<p>Гарантия 2 года</p>
+				<span>Гарантия от производителя</span>
+			</div>
+		</div>
+		<div class="header__contact">
+			<img src="img/header/icon-2.png" alt="">
+			<div class="header__contact-info">
+				<a href="tel:8 800 600-17-10">8 800 600-17-10</a>
+				<a href="#" class="fancy">Обратный звонок</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="big-head-sec s-def">
+	<div class="big-head container">
+		<div class="row">
+			<div class="grid-6">
+				<div class="big-head__info">
+					<div class="big-head__descr">
+						<span>3D принтер</span>
+						<h1>Picaso 3D Designer X Pro</h1>
+						<p>Designer X Pro это 3D принтер для профессионалов, несущий в себе возможность к реализации сложных и подчас невыполнимых задач. Качество печати и скорость выполнения заданий были в приоритете у создателей этого принтера.</p>
+					</div>
+					<div class="big-head__price">
+						<p class="big-head__price-title">Закажите со скидкой 20%</p>
+						<p class="big-head__price-old">370 000 <span class="rub">₽</span></p>
+						<p class="big-head__price-new">270 000 <span class="rub">₽</span></p>
+					</div>
+					<div class="big-head__btns">
+						<a href="#" class="btn btn--red ripple-cont">Купить в 1 клик</a>
+						<a href="#" class="big-head__btn-play">
+							<span class="play-icon"></span>
+							<p>Посмотрите видео</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="grid-6">
+				<div class="big-head__img">
+					<img src="" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-about">
+	<div class="container">
+		<div class="row">
+
+			<div class="grid-5 grid-12_s">
+				<p class="h1 s-about__title">3D принетре Picaso 3D Designer X Pro</p>
+				<div class="user-content">
+					<p>Моментальная смена материалов – печатающая головка JetSwitch при работе в два сопла стала еще быстрее. Смена филамента теперь происходит от 250 мс до 5 с в зависимости от профиля печати.</p>
+					<p>Эталонное качество – при смене материала точность позиционирования сопел достигает 1 микрона, жесткая корпусная рама, модернизированная механика и аппаратная платформа PICASO X.</p>
+					<p>Контроль состояния – функция, позволяющая осуществлять мониторинг за бесперебойностью подачи пластиковой нити в экструдер, состоянием сопла, тем как печатается первый слой и информировать оператора о реальном состоянии принтера.</p>
+					<p>Системный подход к калибровке - автоматическая калибровка рабочей платформы, автоматическая калибровка высоты сопел, калибровка смещения сопел.</p>
+				</div>
+			</div>
+
+			<div class="grid-7 grid-12_s">
+				<div class="slider">
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_2.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_2.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+				</div>
+			</div>
+
+		</div>	
+	</div>
+</section>
+
+
+<section class="s-result">
+	<div class="container">
+		<div class="row">
+
+			<div class="grid-5 grid-12_s">
+				<p class="h1 s-result__title">Результат говорит сам за себя</p>
+				<div class="user-content">
+					<p>В 3D принтере Designer X PRO применяются рельсовые направляющие. Они повышают скорость перемещения экструдера и точность его позиционирования. Использование стальной рамы улучшает жесткость конструкции</p>
+					<p>Технология переключения сопел – JetSwitch, ускоряет процесс создания изделий и экономит ваше время. В качестве эксперимента была отпечатана модель сверла высотой 18 м. Обычный принтер с двумя экструдерами выполнил задание за 37 часов, в то время как Designer X PRO изготовил эту же деталь за 22 часа. И все это благодаря тому, что JetSwitch позволяет производить переключение между материалами происходит за 5 секунд.</p>
+				</div>
+			</div>
+
+			<div class="grid-7 grid-12_s">
+				<div class="slider">
+					<div class="slide">
+						<a href="/img/slide_2.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb-dark.png" data-flickity-lazyload="/img/slide_2.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb-dark.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+				</div>
+			</div>
+
+		</div>	
+	</div>
+</section>
+
+
+<section class="s-preims">
+	<div class="container">
+		<p class="h2 s-preims__title">Преимущества сотрудничества</p>
+
+		<div class="preims">
+			<div class="preim">
+				<header>
+					<img src="/img/preims/1.jpg" alt="1">
+				</header>
+				<div class="preim__title">Бесплатная доставка</div>
+				<div class="preim__descr">осуществляется по&nbsp;РФ до&nbsp;ближайшего к&nbsp;покупателю пункту выдачи транспортной компании.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/2.jpg" alt="1">
+				</header>
+				<div class="preim__title">Специальные условия</div>
+				<div class="preim__descr">На поставки для&nbsp;образовательных организаций.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/3.jpg" alt="1">
+				</header>
+				<div class="preim__title">Обучение в&nbsp;подарок</div>
+				<div class="preim__descr">Обучаем вашего специалиста в&nbsp;офисах компании (Казань и&nbsp;Санкт-Петербург) при&nbsp;покупке оборудования.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/4.jpg" alt="1">
+				</header>
+				<div class="preim__title">Пусконаладка в&nbsp;подарок</div>
+				<div class="preim__descr">Осуществление бесплатных пусконаладочных работ в&nbsp;городах Казань и&nbsp;Санкт-Петербург.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/5.jpg" alt="1">
+				</header>
+				<div class="preim__title">Постоянная скидка до&nbsp;20%</div>
+				<div class="preim__descr">Обучаем вашего специалиста в&nbsp;офисах компании (Казань и Санкт-Петербург) при&nbsp;покупке оборудования.</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+<section class="s-table">
+	<div class="container">
+		<p class="h1 s-table__title">Техническая спецификация</p>
+
+		<div class="tech-table">
+			<div class="tech-row">
+				<div class="tech-row__key">Бренд:</div>
+				<div class="tech-row__value">Picaso</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Страна производитель:</div>
+				<div class="tech-row__value">РОССИЯ</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Габариты товара, см:</div>
+				<div class="tech-row__value">49.2 x 39 x 43</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Габариты в упаковке, см:</div>
+				<div class="tech-row__value">69 х 50 х 47</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Вес нетто, кг:</div>
+				<div class="tech-row__value">16</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Вес брутто, кг:</div>
+				<div class="tech-row__value">20</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Дисплей:</div>
+				<div class="tech-row__value">Да</div>
+			</div>
+			<div class="tech-row">
+				<div class="tech-row__key">Температура хранения, °C:</div>
+				<div class="tech-row__value">0° - 32°С</div>
+			</div>
+
+			<div class="tech-row tech-row--hidden">
+				<div class="tech-row__key">Вес нетто, кг:</div>
+				<div class="tech-row__value">16</div>
+			</div>
+			<div class="tech-row tech-row--hidden">
+				<div class="tech-row__key">Вес брутто, кг:</div>
+				<div class="tech-row__value">20</div>
+			</div>
+			<div class="tech-row tech-row--hidden">
+				<div class="tech-row__key">Дисплей:</div>
+				<div class="tech-row__value">Да</div>
+			</div>
+			<div class="tech-row tech-row--hidden">
+				<div class="tech-row__key">Температура хранения, °C:</div>
+				<div class="tech-row__value">0° - 32°С</div>
+			</div>
+		</div>
+
+		<div class="tech-table__more">
+			<a href="#" rel="nofollow" class="tech-table--show">Смотреть все параметры <i class="i-down"></i></a>
+			<a href="#" rel="nofollow" class="tech-table--hide">Свернуть <i class="i-down"></i></a>
+		</div>
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
