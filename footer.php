299443985c99cb6fe04dfc4a49ca5bc450597a33
Andrew <andreiduffy@gmail.com>
2018-08-09 14:55:52 +0300

    new buttons

--- footer.php
+++ footer.php
@@ -18,6 +18,25 @@
 		</form>
 	</div>
 
+	<div id="modal-order" class="modal">
+		<div class="modal__icon">
+			<img src="/img/modal-order.svg" alt="icon">
+		</div>
+		<p class="h3 modal__title">Оставьте заявку и мы проконсультируем вас</p>
+		<form class="ajax-form vertical-form" autocomplete="off">
+			<input type="hidden" value="Новый заказ. LP:Picaso 3D Designer X Pro" name="form_subject">
+			<input type="text" name="user_name" placeholder="Введите имя*" data-label="Имя пользователя" class="input-text" data-req="true">
+			<input type="tel" name="user_tel" placeholder="Введите телефон*" data-label="Телефон"  class="input-text" data-req="true">
+			<input type="email" name="user_email" placeholder="Введите e-mail*" data-label="E-mail"  class="input-text" data-req="true">
+			<input type="text" name="user_company" placeholder="Название компании" data-label="Название компании" class="input-text">
+			<label class="style-check-ios">
+				<input type="checkbox" name="user_policy" data-label="Согласен с условиями" value="yes" data-req="true" checked="">
+				<span>Даю свое согласие на&nbsp;обработку моих персональных данных и&nbsp;соглашаюсь с&nbsp;<a href="/privacy/" target="_blank">политикой конфиденциальности</a></span>
+			</label>
+			<button type="submit" class="btn btn--red">Отправить</button>
+		</form>
+	</div>
+
 	<div id="modal-thanks" class="modal modal--thanks">
 		<p class="h2">Спасибо за&nbsp;заявку!</p>
 	</div>
